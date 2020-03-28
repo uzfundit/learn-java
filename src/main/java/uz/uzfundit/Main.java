aa09e6b8e244a3e35cbb1f4a47c2cc6f57db8895
jamol <djjamol@gmail.com>
2020-03-28 20:15:43 +0100

    BizzBuzz game function implemented

--- src/main/java/uz/uzfundit/Main.java
+++ src/main/java/uz/uzfundit/Main.java
@@ -8,5 +8,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println(String.format("Hello %s, welcome to the JAVA world!", name));
+        System.out.println("Here is the BizzBuzz game:");
+        bizzBuzz();
+    }
+
+    private static void bizzBuzz(){
+        for(int i = 1; i <= 100; i++){
+            if(i % 15 == 0){
+                System.out.println("BizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Bizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
     }
 }
