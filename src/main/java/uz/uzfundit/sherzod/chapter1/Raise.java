e2657709b99a87b9024cfda2f330c56325ca926e
Sherzod <aliqulovshf@gmail.com>
2020-04-28 17:09:10 +0500

    Another change

--- src/main/java/uz/uzfundit/sherzod/chapter1/Raise.java
+++ src/main/java/uz/uzfundit/sherzod/chapter1/Raise.java
@@ -4,34 +4,23 @@ import java.util.Scanner;
 
 public class Raise {
     public static void main(String[] args) {
-        whileRaising();
-        forRaising();
-    }
-
-    public static void whileRaising(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number: ");
         int a = scanner.nextInt();
         System.out.println("Enter your raise number: ");
         int b = scanner.nextInt();
+        raise(a,b);
+        long result = raise(a,b);
+        System.out.println("Result: "+result);
+    }
+
+    public static long raise(int a, int b){
         long result = 1;
         while (b>0){
             result = result * a;
             b--;
         }
-        System.out.println("Result: "+result);
+        return result;
     }
 
-    public static void forRaising(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your number: ");
-        int a = scanner.nextInt();
-        System.out.println("Enter your raise number: ");
-        int b = scanner.nextInt();
-        int i;
-        long result = 1;
-        for(i=1; i<=b; i++)
-            result = result * a;
-        System.out.println("Result: "+result);
-    }
 }
